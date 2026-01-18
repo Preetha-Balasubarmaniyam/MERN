0ba981c7722136a401192cacc6c84d228b9d0e3a
madhu <madhuth.tech@gmail.com>
2026-01-18 18:27:32 +0530

    Assignment 7 added

---
+++ React/reactBundler/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const headingElement= React.createElement("h1",null,"React using Parcel Bundler");
+const Root= ReactDOM.createRoot(document.getElementById("root"));
+Root.render(headingElement);
---
+++ React/reactVite/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const paraElement = <h1>Hello JSX....</h1>
+const Root= ReactDOM.createRoot(document.getElementById("root"));
+Root.render(paraElement);
